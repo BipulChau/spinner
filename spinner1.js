9494cae5ace978adb40aaacca0c03fed27341e3b
Bipul Chaudhary <bipulchau@gmail.com>
2021-09-21 14:25:43 -0400

    Spinner1

---
+++ spinner1.js
@@ -0,0 +1,31 @@
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 1000);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 2000);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 3000);
+
+setTimeout(() => {
+  process.stdout.write('\r\\   ');
+}, 4000);
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 5000);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 6000);
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 7000);
+
+setTimeout(() => {
+  process.stdout.write('\n');
+}, 7500);
